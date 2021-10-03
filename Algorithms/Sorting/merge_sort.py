a372f63d276dc16dd60dcf20cf808e4fee605f9e
Allwin Raju <allwindicaprio@gmail.com>
2021-10-03 21:44:43 +0530

    Create merge_sort.py

---
+++ Algorithms/Sorting/merge_sort.py
@@ -0,0 +1,35 @@
+"""
+Merge sort algorithm using recursive function (Divide and Conquer strategy)
+"""
+
+def merge(left, right):
+    # Merge function
+    left_index, right_index = 0, 0
+    result = []
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+
+def merge_sort(array):
+    # Recursive function to divide and merge
+    if len(array) <= 1:  # base case
+        return array
+
+    # divide array in half and merge sort recursively
+    half = len(array) // 2
+    left = merge_sort(array[:half])
+    right = merge_sort(array[half:])
+
+    return merge(left, right)
+
+
+li = [2, 1, 3, 10, 8]
